f131db0b643126c03b7acf481c438ea8b6b44020
paralelod <hello.diogorusso@gmail.com>
2018-02-08 22:11:40 -0200

    fix model bug

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,21 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
     def __str__(self):
         return self.title 
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=())
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
