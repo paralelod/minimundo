6dd71174aa4f08628ae117b2fdd74a8cc1bbc207
paralelod <hello.diogorusso@gmail.com>
2018-02-09 08:01:44 -0200

    postgres

--- minimundo/settings.py
+++ minimundo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'minimundo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': diogorusso.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'diogorusso',
         'USER': 'diogorusso',
         'PASSWORD': '',
